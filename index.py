774c04513d6478babaec6a84015361359f2178d2
Yahir Garcia <yahirgarcia1996@gmail.com>
2021-05-28 13:30:31 -0500

    Pickle implementado, filtros modificados

--- index.py
+++ index.py
@@ -1,15 +1,12 @@
-from typing import runtime_checkable
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, make_response
 from werkzeug.utils import secure_filename
-import os
-import modulo1 
+from typing import *
 import modulo2
-import modulo3
-import conversacion  
+import pdfkit
+import os
+
+# lista que almacena lo que retorna modulo2
 res = []
-# print('arca',hex(id(res)))
-# res1 = conversacion.archivo()
-res2 = modulo2.iniciar()
 
 # objeto apara crear rutas
 app = Flask(__name__)
@@ -42,26 +39,60 @@ def cargar_ac():
 # simulacion de chat
 @app.route('/simulacion-chat')
 def simulacion_chat():
-    return render_template('simulacion-chat.html',resarchivo=res)
+    return render_template('simulacion-chat.html',datos=res)
+
+#elige que filtrar
+@app.route('/elegir-filtros',methods=['POST','GET'])
+def elegir_filtros():
+    return render_template('elegir-filtros-rs.html')
+
+#elige que bloquear
+@app.route('/elegir-bloqueos',methods=['POST','GET'])
+def elegir_bloqueos():
+    return render_template('elegir-bloqueos-rs.html')
+
+# resultados de la eleccion de filtros
+@app.route('/resultado-filtros', methods=['POST','GET'])
+def resultado_filtros():
+    if request.method=='POST':
+        opciones = request.form.getlist('filtrosm')
+    return render_template('resultado_filtros.html',datos=res, op=opciones)
+
+# resultados de lo que bloqueo
+@app.route('/resultado-filtros-bloqueados', methods=['POST','GET'])
+def resultado_filtros_bloqueados():
+    if request.method=='POST':
+        opciones = request.form.getlist('bloqueos')
+    return render_template('resultado_filtros_bloqueados.html',datos=res, op=opciones)
 
 # muestra mensajes agresivos
 @app.route('/mensajes-agresivos')
 def filtro_msj_agr():
-    return render_template('filtro-msj-agr.html',resModulo1=res)
+    return render_template('filtro-msj-agr.html',datos=res)
 
 # bloquea mensajes agresivos
 @app.route('/bloqueaMsj')
 def bloquea():
-    return render_template('bloqueaMsj.html', resModulo2=res)
+    return render_template('bloqueaMsj.html', datos=res)
 
 # reporte de agresion gv
 @app.route('/reporte-gv')
 def make_reportGV():
-    return render_template('reporte-gv.html',data=res)
+    return render_template('reporte-gv.html',datos=res)
 
 @app.route('/reporte-dw')
 def make_reportDW():
-    return render_template('reporte-dw.html',data=res)
+    return render_template('reporte-dw.html',datos=res)
+
+# descargar el PDF
+@app.route('/descargar')
+def dw_reporte():
+    options = {"enable-local-file-access": None}
+    path_wkhtmltopdf = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    f=open("./templates/reporte-dw.html","r")
+    pdf = pdfkit.from_file(f,'out.pdf', configuration=config, options=options)
+    return render_template('reporte-dw.html')
 
 # pagina de contacto
 @app.route('/contacto')
@@ -81,8 +112,8 @@ def uploader():
         filename = secure_filename(f.filename)
         # Guardamos el archivo en el directorio "Archivos"
         f.save(os.path.join(app.config['UPLOAD_FOLDER'],'archivo.json'))
-        msg='se subio el archivo correctamente'
-        res.append(modulo1.iniciar())
+        msg='Archivo cargado con exito'
+        res.append(modulo2.iniciar())
         for elem in res:
             print(elem)
         if tipo == 'rs':
--- modulo2.py
+++ modulo2.py
@@ -1,4 +1,4 @@
-import numpy as np
+'''import numpy as np
 import pandas as pd
 import re
 import nltk
@@ -14,11 +14,15 @@ from sklearn.metrics import classification_report
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-import pprint
-
-
+#from jinja2 import Environment, FileSystemLoader
+import pdfkit
+import os
+from flask import render_template, make_response
+from fpdf import Template
 def iniciar():
-    train = pd.read_csv('static/files/train_aggressiveness.csv', encoding = 'utf-8')
+    
+    export=[]
+    train = pd.read_csv('./static/archivos/train_aggressiveness.csv', encoding='utf-8')
     df = train.copy()
     # df.head()
 
@@ -80,30 +84,49 @@ def iniciar():
     # print(df["prediccion"])
 
     df['Determinante'] = df['prediccion'].apply(lambda prediccion:'No agresivo' if prediccion ==0 else 'Agresivo')
-
+    with open('text_classifier', 'wb') as picklefile:
+        pickle.dump(model,picklefile)
+    
+    #with open('text_classifier', 'rb') as training_model:
+    #    model = pickle.load(training_model)
     # print(df.head(15))
 
     # Opening JSON file
-    f = open('static/files/conversation001.json', encoding="utf8")
-
-    # returns JSON object as
-    # a dictionary
-    data = json.load(f)
-
-    # Iterating through the json
-    # list
     mensajes=[]
-    for i in data['messages']:
-    # print(i['text'])
-        menU = (i['from'])
-        tex = (i['text'])
-        men=(menU+tex)
-        mensajes.append(men)
-        #mensajes.append(i['text'])
-    # Closing file
-    f.close()
+    conv_users = []
+    try:
+        f = open('./static/archivos/archivo.json', encoding="utf8")
+        
+        # returns JSON object as
+        # a dictionary
+        data = json.load(f)
 
-    mensajes_nolist=[]
+        # Iterating through the json
+        # list
+        for i in data['messages']:
+            # print(i['text'])
+            mensajes.append(i)
+
+        
+            
+        # print(len(mensajes))
+        cont = 0
+        # print(mensajes)
+        
+        for i in mensajes:
+            if i['text'] == "":
+                mensajes.pop(cont)
+            cont += 1
+        for elem in mensajes:
+            if elem['type'] == 'message':
+                if elem['from'] not in conv_users:
+                    conv_users.append(elem['from'])
+
+        f.close()
+    except:
+        print('errorXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+
+    """mensajes_nolist=[]
     # print("imp\n", mensajes)
     for elem in mensajes:
         if type(elem) == str:
@@ -114,34 +137,277 @@ def iniciar():
                 # process_text(elem2)
                 mensajes_nolist.append(elem2)
         else:
-            continue
+            continue"""
 
     determinantes = []
-    mensajes_Noagresivos = []
-    for elem in mensajes_nolist:
-        if type(elem)==str:
-            # print(elem)
-            determinante = model.predict(vect.transform([elem]))
-            if determinante == 0:
-                mensajes_Noagresivos.append(elem)
-            determinantes.append(determinante)
+    mensajes_agresivos = []
+    mensajes_no_agresivos = []
 
+
+    for elem in mensajes:
+        aux = elem['text']
+        determinante = model.predict(vect.transform([aux]))
+        if determinante == 1:
+            mensajes_agresivos.append(elem)
+        elif determinante == 0:
+            mensajes_no_agresivos.append(elem)
+        determinantes.append(determinante)
+    print('dt:',determinantes)
     contador = 0
-    for elem in determinantes:
+    """for elem in determinantes:
         if elem == 0:
             tipo = 'NO AGRESIVO'
             print('El mensaje ',contador,' es ',tipo)
         else:
             tipo = 'AGRESIVO'
             print('El mensaje ',contador,' es ',tipo)
-        contador = contador + 1
+        contador = contador + 1"""
 
-    with open('text_classifier', 'wb') as picklefile:
-        pickle.dump(model,picklefile)
+    class Objeto:
+        nombre = ""
+        id = ""
+        cantidad = 0
+        totalmsj = 0
+        # msj_agr = []
+    
+    usuarios_agresivos=[]
+    # recorremos la lista de mensajes agresivos
+    for e in mensajes_agresivos:
+        # verificamos que no se repitan los nombres
+        if e['from'] not in usuarios_agresivos:
+        # se agrega el user a la lista
+            usuarios_agresivos.append(e['from'])
 
-    return mensajes_Noagresivos
+    print(usuarios_agresivos)
+    users = []
+    # recorremos la lista de user agresivos
+    for e in usuarios_agresivos:
+        # se crea objeto
+        o = Objeto()
+        # asignacion de nombre
+        o.nombre = str(e)
+        # cantidad por default se pone en 0
+        # el objeto se añade a la lista users
+        users.append(o)
+        for f in mensajes_agresivos:
+            if f['from'] == e:
+                o.id = f['from_id']
+                break
+        print('Objeto ',e, 'creado')
+    
+    cont = 0
+    # recorremos la lista de msj agresivos
+    for d in mensajes_agresivos:
+        # recorremos la lista de objetos
+        for h in users:
+        # verificamos si el usuario agresivo tiene un objeto creado
+            if d['from'] == h.nombre:
+                # se aumenta en uno la cantidad de msj agr
+                h.cantidad+=1
+    
+    print('Users')
+    # recorremos la lista de objetos
+    for h in users:
+        print('User: ',h.nombre,' Num.MsjAgr: ',h.cantidad)
+
+    for p in mensajes:
+        for q in users:
+            if p['from'] == q.nombre:
+                q.totalmsj+=1
+    interna=[]
+    interna.append(mensajes_agresivos)
+    interna.append(conv_users)
+    interna.append(mensajes)
+    interna.append(mensajes_no_agresivos)
+    interna.append(users)
+    export.append(interna)
+    
+    
+    listadic=[]
+    for persona in users:  
+        diccionario = {'nombre' : persona.nombre, 'id' : persona.id, 'nma': persona.cantidad, 'msjt': persona.totalmsj }
+        listadic.append(diccionario)
+    
+    #env = Environment(loader=FileSystemLoader("templates"))
+    #template= env.get_template("reporte-dw.html")
 
     
+    #html = render_template('reporte-dw.html')
+    #response = make_response(pdf)
+    #response.headers["Content-Type"] = "application/pdf"
+    #response.headers["Content-Disposition"] = "inline; filename=output.pdf"
+    #here we instantiate the template and define the HEADER
+    
+   
+    #and now we render the page
+    blockUsers = []
+    userSlice  = slice(3)
+    for o in users:
+        if o.cantidad >= 2:
+            blockUsers.append(o)
+            #bloqueaUsers= blockUsers[userSlice]
+    
+    #print(bloqueaUsers)
 
+    #resultado[0][3]
+    msjAgrUs = []
+    for w in blockUsers:
+        for q in mensajes_agresivos:
+            if q['from']==w.nombre:
+                msjAgrUs.append(q['text'])
     
 
+    #print(msjAgrUs)
+    
+    return mensajes_agresivos, conv_users, mensajes, mensajes_no_agresivos, users,usuarios_agresivos,blockUsers,msjAgrUs
+'''
+import json
+import joblib
+
+model=joblib.load('model.pkl')
+vect=joblib.load('vectorizer.pkl')
+
+def iniciar():
+
+    mensajes=[]
+    conv_users = []
+
+    try:
+        f = open('./static/archivos/archivo.json', encoding="utf8")
+        
+        # returns JSON object as
+        # a dictionary
+        data = json.load(f)
+
+        # Iterating through the json
+        # list
+        for i in data['messages']:
+            # print(i['text'])
+            mensajes.append(i)
+
+        
+            
+        # print(len(mensajes))
+        cont = 0
+        # print(mensajes)
+        
+        for i in mensajes:
+            if i['text'] == "":
+                mensajes.pop(cont)
+            cont += 1
+        for elem in mensajes:
+            if elem['type'] == 'message':
+                if elem['from'] not in conv_users:
+                    conv_users.append(elem['from'])
+
+        f.close()
+    except:
+        print('Error. No se pudo abrir el archivo.')
+
+    determinantes = []
+    mensajes_agresivos = []
+    mensajes_no_agresivos = []
+
+
+    for elem in mensajes:
+        aux = elem['text']
+        determinante = model.predict(vect.transform([aux]))
+        if determinante == 1:
+            mensajes_agresivos.append(elem)
+        elif determinante == 0:
+            mensajes_no_agresivos.append(elem)
+        determinantes.append(determinante)
+    print('dt:',determinantes)
+    contador = 0
+
+    class Objeto:
+        nombre = ""
+        id = ""
+        #cantidad (de mensajes agresivos)
+        cantidad = 0
+        totalmsj = 0
+        # msj_agr = []
+    
+    usuarios_agresivos=[]
+    # recorremos la lista de mensajes agresivos
+    for e in mensajes_agresivos:
+        # verificamos que no se repitan los nombres
+        if e['from'] not in usuarios_agresivos:
+        # se agrega el user a la lista
+            usuarios_agresivos.append(e['from'])
+
+    print(usuarios_agresivos)
+    users = []
+    # recorremos la lista de user agresivos
+    for e in usuarios_agresivos:
+        # se crea objeto
+        o = Objeto()
+        # asignacion de nombre
+        o.nombre = str(e)
+        # cantidad por default se pone en 0
+        # el objeto se añade a la lista users
+        users.append(o)
+        for f in mensajes_agresivos:
+            if f['from'] == e:
+                o.id = f['from_id']
+                break
+        print('Objeto ',e, 'creado')
+    
+    cont = 0
+    # recorremos la lista de msj agresivos
+    for d in mensajes_agresivos:
+        # recorremos la lista de objetos
+        for h in users:
+        # verificamos si el usuario agresivo tiene un objeto creado
+            if d['from'] == h.nombre:
+                # se aumenta en uno la cantidad de msj agr
+                h.cantidad+=1
+    
+    print('Users')
+    # recorremos la lista de objetos
+    for h in users:
+        print('User: ',h.nombre,' Num.MsjAgr: ',h.cantidad)
+
+    for p in mensajes:
+        for q in users:
+            if p['from'] == q.nombre:
+                q.totalmsj+=1
+    
+    
+    listadic=[]
+    for persona in users:  
+        diccionario = {'nombre' : persona.nombre, 'id' : persona.id, 'nma': persona.cantidad, 'msjt': persona.totalmsj }
+        listadic.append(diccionario)
+    
+    #env = Environment(loader=FileSystemLoader("templates"))
+    #template= env.get_template("reporte-dw.html")
+
+    
+    #html = render_template('reporte-dw.html')
+    #response = make_response(pdf)
+    #response.headers["Content-Type"] = "application/pdf"
+    #response.headers["Content-Disposition"] = "inline; filename=output.pdf"
+    #here we instantiate the template and define the HEADER
+    
+   
+    #and now we render the page
+    blockUsers = []
+    userSlice  = slice(3)
+    for o in users:
+        if o.cantidad >= 2:
+            blockUsers.append(o)
+            #bloqueaUsers= blockUsers[userSlice]
+    
+    #print(bloqueaUsers)
+
+    #resultado[0][3]
+    msjAgrUs = []
+    for w in blockUsers:
+        for q in mensajes_agresivos:
+            if q['from']==w.nombre:
+                msjAgrUs.append(q['text'])
+    
+
+    #print(msjAgrUs)
+
+    return mensajes_agresivos, conv_users, mensajes, mensajes_no_agresivos, users,usuarios_agresivos,blockUsers,msjAgrUs
